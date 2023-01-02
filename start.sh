389231e0975f98bee7e9946dfa3f59234639ce6b
Jnanesh12 <108192234+Jnanesh12@users.noreply.github.com>
2023-01-02 11:37:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Jnanesh12/jna-mdisc-sesrch-bita-bot /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Jnanesh12/jna-mdisc-sesrch-bita-bot /jna-mdisc-sesrch-bita-bot
+cd /jna-mdisc-sesrch-bita-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
